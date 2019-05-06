6771478eaf066659eae9af7f87e3c64a72025547
Cong Ma <cong.ma@uct.ac.za>
2019-05-06 11:47:40 +0200

    TST, DOC: test partial_derivative method for spherical bivariate spline.
    Also removes spurious reference to the private class
    "_DerivedBivariateSpline" in docstrings.

--- scipy/interpolate/tests/test_fitpack2.py
+++ scipy/interpolate/tests/test_fitpack2.py
@@ -525,6 +525,13 @@ class TestRectSphereBivariateSpline(object):
         assert_allclose(lut(x, y, dtheta=1, dphi=1), _numdiff_2d(lut, x, y, dx=1, dy=1, eps=1e-6),
                         rtol=1e-3, atol=1e-3)
 
+        assert_array_equal(lut(x, y, dtheta=1),
+                           lut.partial_derivative(1, 0)(x, y))
+        assert_array_equal(lut(x, y, dphi=1),
+                           lut.partial_derivative(0, 1)(x, y))
+        assert_array_equal(lut(x, y, dtheta=1, dphi=1),
+                           lut.partial_derivative(1, 1)(x, y))
+
     def test_derivatives(self):
         y = linspace(0.01, 2*pi-0.01, 7)
         x = linspace(0.01, pi-0.01, 7)
@@ -547,6 +554,13 @@ class TestRectSphereBivariateSpline(object):
         assert_allclose(lut(x, y, dtheta=1, dphi=1, grid=False),
                         _numdiff_2d(lambda x,y: lut(x,y,grid=False), x, y, dx=1, dy=1, eps=1e-6),
                         rtol=1e-3, atol=1e-3)
+
+        assert_array_equal(lut(x, y, dtheta=1, grid=False),
+                           lut.partial_derivative(1, 0)(x, y, grid=False))
+        assert_array_equal(lut(x, y, dphi=1, grid=False),
+                           lut.partial_derivative(0, 1)(x, y, grid=False))
+        assert_array_equal(lut(x, y, dtheta=1, dphi=1, grid=False),
+                           lut.partial_derivative(1, 1)(x, y, grid=False))
 
 
 def _numdiff_2d(func, x, y, dx=0, dy=0, eps=1e-8):
